274f6eeaa280d31d32a6ec17037a62b2f74cb52f
penelopem2 <penelope.m2@yandex.com>
2015-12-27 21:25:10 -0500

    Update winmain.cpp

--- winmain.cpp
+++ winmain.cpp
@@ -9,6 +9,7 @@ public:
 	void sayHello(){
 		cout << "Hello World" << endl;
 	}
+	/*906B8B34DC78AE8E1F46BE8583D508A7AEAFEAEA13C840C1B43F5621C4999E567F2D2749C6D1379A337552F98247*/
 };
 
 int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow ){
